9b82e82d39641cfb0ed8552dd1807e6b6bc22762
User-PC\User <cilliancleland@gmail.com>
2017-05-06 08:10:57 +1000

    no message

--- app/script.js
+++ app/script.js
@@ -1,46 +1,91 @@
 "use strict";
-var swissChess = angular.module('swissChess', []);
+let swissChess = angular.module('swissChess', []);
 
-swissChess.factory("competitionFactory", [function(){
-  let comp = {};
+swissChess.factory("playersFactory", [function(){
+  let players = {};
+  const playersFactory = {};
 
+  playersFactory.sortPlayers = function sortPlayers(currentOrder, sortBy){
+    return currentOrder.sort((a,b) => {
+      return players[b][sortBy] - players.getPlayerById[a][sortBy];
+    });
+  };
+
+  playersFactory.shufflePlayers = function shufflePlayers(currentOrder) {
+    let modifier = currentOrder.length % 2 ? 1 : 2;//if an odd number of players, we need to adjust our shuffle, else the last player never gets a bye
+    currentOrder.unshift(currentOrder[currentOrder.length-modifier]);
+    currentOrder.splice(currentOrder.length-modifier, 1)
+  };
+
+  playersFactory.initPlayers = function initPlayers(numPlayers) {
+    for(let i=0;i<numPlayers;i++){
+      players[i+1]={id:i+1,score:0,countBack:0};
+    }
+  };
+
+  playersFactory.resetPlayers = function resetPlayers() {
+    players = {};
+  };
+
+  playersFactory.setAllPlayers = function setAllPlayers(newPlayers) {
+    players = Object.assign({},newPlayers);
+  };
   
+  playersFactory.getAllPlayers = function getAllPlayers() {
+    return players;
+  };
 
+  playersFactory.getPlayerById = function getPlayer(id) {
+    return players[id];
+  }
 
+  return playersFactory;
 }]);
 
-swissChess.controller("mainCtrl",["$scope",
-function($scope){
+swissChess.controller("mainCtrl",["$scope", "playersFactory",
+function($scope,players){
   const POINTS_FOR_WIN = 3;
   const POINTS_FOR_BYE = 3;
   const POINTS_FOR_DRAW = 1;
-  const SAVE_PROMPT = "Are you sure, this will overwrite your saved game.\n\nPress ok to continue.";
-  const LOAD_PROMPT = "Are you sure, this will overwrite all details of your current game.\n\nPress ok to continue.";
-  const DELETE_PROMPT = "Are you sure, this will deltee your saved game.\n\nPress ok to continue.";
+  const SAVE_PROMPT = "Are you sure, this will overwrite your saved competition.\n\nPress ok to continue.";
+  const LOAD_PROMPT = "Are you sure, this will overwrite all details of your current competition.\n\nPress ok to continue.";
+  const DELETE_PROMPT = "Are you sure, this will delete your saved competition.\n\nPress ok to continue.";
+  const RESET_PROMPT = "Are you sure, this will clear your current competition.\n\nPress ok to continue.";
   const SAVED_GAME_NAME = "savedComp";
   
-  $scope.reset = function reset(){
+  $scope.init = function init() {
     $scope.initialised = false;
+    $scope.completed = false;
+    $scope.currentRoundNumber = "0";
     $scope.numPlayers = 0;
     $scope.numRounds = 0;
     $scope.title = '';
     $scope.rounds = {};
-    $scope.currentRoundNumber = "0";
-    $scope.players={};
+    players.resetPlayers();
     $scope.activeTab = "";
-    $scope.completed = false;
     $scope.playerOrder = [];
     let savedGame = localStorage.getItem(SAVED_GAME_NAME);
     if(savedGame){
       let savedObj = JSON.parse(savedGame);
-      $scope.saved = `Competition ${savedObj.title} (${savedObj.numRounds} rounds : ${savedObj.numPlayers} players) saved on ${savedObj.savedOn}`;
+      $scope.saved = getSavedDetails(savedObj);
     }
+  }
+
+  $scope.reset = function reset(){
+    if(!confirm(RESET_PROMPT)){
+      return;
+    }
+    $scope.init();
   };
 
+  function getSavedDetails(savedObj) {
+    return `Competition ${savedObj.title} (${savedObj.numRounds} rounds : ${savedObj.numPlayers} players) saved on ${savedObj.savedOn}`;
+  }
+
   $scope.deleteSave = function deleteSave(){
-      if(!confirm(DELETE_PROMPT)){
-        return;
-      }  
+    if(!confirm(DELETE_PROMPT)){
+      return;
+    }
     localStorage.removeItem(SAVED_GAME_NAME);
     $scope.saved = false;
   };
@@ -58,17 +103,17 @@ function($scope){
     saveObj.title = $scope.title;
     saveObj.rounds = $scope.rounds;
     saveObj.currentRoundNumber = $scope.currentRoundNumber;
-    saveObj.players = $scope.players;
+    saveObj.players = players.getAllPlayers();
     saveObj.activeTab =  $scope.activeTab;
     saveObj.completed = $scope.completed;
     saveObj.playerOrder = $scope.playerOrder;
     saveObj.savedOn = Date.now();
     localStorage.setItem(SAVED_GAME_NAME, JSON.stringify(saveObj));
-    $scope.saved = true;
+    $scope.saved =getSavedDetails(saveObj);
   };
 
   $scope.load = function load(){
-    if(!confirm(LOAD_PROMPT)){
+    if($scope.initialised && !confirm(LOAD_PROMPT)){
       return;
     }
     let saveStr = localStorage.getItem(SAVED_GAME_NAME);
@@ -80,27 +125,29 @@ function($scope){
     $scope.title= saveObj.title;
     $scope.rounds= saveObj.rounds;
     $scope.currentRoundNumber= saveObj.currentRoundNumber;
-    $scope.players= saveObj.players;
+    debugger;
+    players.setAllPlayers( saveObj.players);
     $scope.activeTab= saveObj.activeTab;
     $scope.completed= saveObj.completed;
     $scope.playerOrder= saveObj.playerOrder;
 
   };
 
+  $scope.getAllPlayers = function getAllPlayers() {
+    return players.getAllPlayers();
+  };
 
   $scope.getStarted = function getStarted(numPlayers,numRounds,title){
     $scope.numPlayers = numPlayers;
     $scope.numRounds = numRounds;
     $scope.title = title;
     $scope.initialised = true;
-    for(let i=0;i<numPlayers;i++){
-      $scope.players[i+1]={id:i+1,score:0,countBack:0};
-    }
+    players.initPlayers(numPlayers);
     $scope.activeTab = "players"
   }
   
   $scope.setupFirstRound = function setupFirstRound() {
-    $scope.playerOrder = Object.keys($scope.players);
+    $scope.playerOrder = Object.keys(players.getAllPlayers());
     $scope.setupRound("1");
   }
   
@@ -108,7 +155,7 @@ function($scope){
     $scope.currentRoundNumber = newRoundNumber;
     $scope.rounds[newRoundNumber] = {roundNumber:newRoundNumber,games:{}};
     let currentRound = $scope.rounds[$scope.currentRoundNumber];
-    shufflePlayers();
+    players.shufflePlayers($scope.playerOrder);
     
     let numGames = $scope.playerOrder.length /2;
     
@@ -128,8 +175,8 @@ function($scope){
 
     for(let gameId in round.games){
       let game = round.games[gameId];
-      let player1 =  $scope.players[game.player1];
-      let player2 =  $scope.players[game.player2];
+      let player1 =  players.getPlayerById(game.player1);
+      let player2 =  players.getPlayerById(game.player2);
       switch (game.result) {
         case "Player 1 win":
           player1.score += POINTS_FOR_WIN;
@@ -147,11 +194,7 @@ function($scope){
     }
     
     if($scope.numRounds == $scope.currentRoundNumber) {
-      generateCountBack();
-      $scope.finalOrder = sortPlayers(Object.keys($scope.players), "countBack");
-      $scope.finalOrder = sortPlayers($scope.finalOrder, "score");
-      $scope.currentRoundNumber = 0;
-      $scope.completed = true;
+      completeComp();
       $scope.activeTab = "results";
     }else{
       let nextRoundNumber = parseInt($scope.currentRoundNumber) + 1;
@@ -159,13 +202,21 @@ function($scope){
     }
   }
 
+  function completeComp() {
+      generateCountBack();
+      $scope.finalOrder = players.sortPlayers(Object.keys(players.getAllPlayers()), "countBack");
+      $scope.finalOrder = players.sortPlayers($scope.finalOrder, "score");
+      $scope.currentRoundNumber = 0;
+      $scope.completed = true;
+  }
+
   function generateCountBack() {    
     for(let roundNumber in $scope.rounds){
       let round = $scope.rounds[roundNumber];
       for(let gameNumber in round.games){
         let game = round.games[gameNumber];
-        let player1 =  $scope.players[game.player1];
-        let player2 =  $scope.players[game.player2];
+        let player1 =  players.getPlayerById(game.player1);
+        let player2 =  players.getPlayerById(game.player2);
         switch (game.result) {
           case "Player 1 win":
             player1.countBack += player2.score;
@@ -183,23 +234,11 @@ function($scope){
     }
   }
 
-  function shufflePlayers() {
-    let modifier = $scope.playerOrder.length % 2 ? 1 : 2;//if an odd number of players, we need to adjust our shuffle, else the last player never gets a bye
-    $scope.playerOrder.unshift($scope.playerOrder[$scope.playerOrder.length-modifier]);
-    $scope.playerOrder.splice($scope.playerOrder.length-modifier, 1)
-  }
-  
-  function sortPlayers(playersArr, sortBy){
-    return playersArr.sort((a,b) => {
-      return $scope.players[b][sortBy] - $scope.players[a][sortBy];
-    });
-  }
-  
   $scope.showTab = function showTab(tab) {
     $scope.activeTab = tab;
   }
 
-  $scope.reset();
+  $scope.init();
 }]);
 
 swissChess.directive("scMenu", function() {
@@ -219,7 +258,7 @@ swissChess.directive("scSetup", function() {
   };
 });
 
-swissChess.directive("scPlayerDetails", function() {    
+swissChess.directive("scPlayerDetails", function() {  
   return {
     type: "E",
     scope: {
---
+++ app/script_test.js
@@ -0,0 +1,44 @@
+var mock, notify;
+beforeEach(module('swissChess'));
+beforeEach(function() {
+//   mock = {alert: jasmine.createSpy()};
+
+//   module(function($provide) {
+//     $provide.value('$window', mock);
+//   });
+
+  inject(function($injector) {
+    playersFactory = $injector.get('playersFactory');
+  });
+});
+
+it('should start with an empty players object', function() {
+  expect(playersFactory.players).toEqual({});
+});
+
+// it('should not alert first two notifications', function() {
+//   notify('one');
+//   notify('two');
+
+//   expect(mock.alert).not.toHaveBeenCalled();
+// });
+
+// it('should alert all after third notification', function() {
+//   notify('one');
+//   notify('two');
+//   notify('three');
+
+//   expect(mock.alert).toHaveBeenCalledWith("one\ntwo\nthree");
+// });
+
+// it('should clear messages after alert', function() {
+//   notify('one');
+//   notify('two');
+//   notify('third');
+//   notify('more');
+//   notify('two');
+//   notify('third');
+
+//   expect(mock.alert.calls.count()).toEqual(2);
+//   expect(mock.alert.calls.mostRecent().args).toEqual(["more\ntwo\nthird"]);
+// });
